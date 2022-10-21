c5e762d26c92b2133b5efae07ae866e27bfe1274
awadh <awadh.hassan@student.moringaschool.com>
2022-10-21 11:13:51 +0300

    done

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,18 @@
+ class Book
+    def initialize(title)
+        @title = title
+    end
+    def title
+        @title
+    end
+
+    attr_writer:author,:page_count,:genre
+    attr_reader:author,:page_count,:genre
+
+    def turn_page
+        puts"Flipping the page...wow, you read fast!"
+    end
+end
+
 
 
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,16 @@
 # Make your shoe class here!
+class Shoe
+    def initialize(brand)
+        @brand = brand
+    end
+def brand 
+    @brand
+end
+attr_reader :brand, :color, :size, :material, :condition
+attr_writer :brand, :color, :size, :material, :condition
+
+def cobble
+    puts "Your shoe is as good as new!"
+    @condition ="new"
+   end
+end  
